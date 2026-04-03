17bec01c91f6994a8aa77aaf3a505925957b3b68
irfi123 <irfu14j@gmail.com>
2026-04-02 21:33:34 -0400

    2026-04-02 21:33

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            transform.SetPositionAndRotation(originalPosition, originalRotation);
+        }
+    }
+}
